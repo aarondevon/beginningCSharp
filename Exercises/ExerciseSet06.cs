debca103f9d828fea2791c75fa8f37a4d03316d2
Aaron Sawyer <me@aarondevon.com>
2019-05-30 15:38:46 -0500

    Calculate the factorial of an int.

--- Exercises/ExerciseSet06.cs
+++ Exercises/ExerciseSet06.cs
@@ -37,5 +37,16 @@ namespace Exercises
             
             Console.WriteLine($"Total: {total}");
         }
+
+        // 03 - Write a program and ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+        public static void ComputeFactorialOfInt(int number)
+        {
+            int factorial = number;
+            for (var i = number - 1; i >= 1; i--)
+            {
+                factorial *= i;
+            }
+            Console.WriteLine($"The factorial of {number} is: {factorial}");
+        }
     }
 }
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -12,6 +12,10 @@ namespace Exercises
 
             // 02 - Ask the user to enter a number or "ok" to exit. Calculates the sum of all the previously entered numbers and displays it on the console.
             ExerciseSet06.AskUserToEnterNumber();
+
+            // 03 - Ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+
+            ExerciseSet06.ComputeFactorialOfInt(5);
         }
     }
 }
