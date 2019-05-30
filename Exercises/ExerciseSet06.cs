6bb4809643fe5952ac14117ed02dcdbc364f8981
Aaron Sawyer <me@aarondevon.com>
2019-05-30 15:24:40 -0500

    Ask user for a number and then display total of numbers to the console once the user exits the loop.

--- Exercises/ExerciseSet06.cs
+++ Exercises/ExerciseSet06.cs
@@ -19,5 +19,23 @@ namespace Exercises
             }
             return total;
         }
+
+        // 02 - Write a program and continuously ask the user to enter a number or "ok" to exit. Calculate the sum of all the previously entered numbers and display it on the console.
+        public static void AskUserToEnterNumber()
+        {
+            int total = 0;
+            do
+            {
+                Console.WriteLine("Enter a number, or enter 'ok' to exit.");
+                string input = Console.ReadLine();
+                if (input == "ok")
+                {
+                    break;
+                }
+                total += Convert.ToInt32(input);
+            } while (true);
+            
+            Console.WriteLine($"Total: {total}");
+        }
     }
 }
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -10,6 +10,8 @@ namespace Exercises
             // 01 Write to the console the total numbers between 1 and 100 that are divisible by 3 with no remainder.
             Console.WriteLine(ExerciseSet06.TotalNumbersDivisibleByThree(100));
 
+            // 02 - Ask the user to enter a number or "ok" to exit. Calculates the sum of all the previously entered numbers and displays it on the console.
+            ExerciseSet06.AskUserToEnterNumber();
         }
     }
 }
