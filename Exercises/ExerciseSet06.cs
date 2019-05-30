c6011ce817040722e493d3382d56efa8044123a6
Aaron Sawyer <me@aarondevon.com>
2019-05-30 14:57:15 -0500

    Create method to find how many numbers are evenly divisible by three given a range from 1 to a user selected number

--- Exercises/ExerciseSet06.cs
+++ Exercises/ExerciseSet06.cs
@@ -6,5 +6,18 @@ namespace Exercises
 {
     class ExerciseSet06
     {
+        // 01 - Write a program to count how many numbers between 1 and a number that are divisible by 3 with no remainder.Display the count on the console.
+        public static int TotalNumbersDivisibleByThree(int topNumberRange)
+        {
+            int total = 0;
+            for (var i = 1; i <= topNumberRange; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    total += 1;
+                }
+            }
+            return total;
+        }
     }
 }
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -6,7 +6,10 @@ namespace Exercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Exercise Set 06*************************************
+            // 01 Write to the console the total numbers between 1 and 100 that are divisible by 3 with no remainder.
+            Console.WriteLine(ExerciseSet06.TotalNumbersDivisibleByThree(100));
+
         }
     }
 }
