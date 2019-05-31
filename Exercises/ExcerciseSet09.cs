a460214ae3f0dcb9ba3ff56fbc14ecf34651ea8f
Aaron Sawyer <me@aarondevon.com>
2019-05-31 08:28:42 -0500

    Add additional exercise sets

---
+++ Exercises/ExcerciseSet09.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Exercises
+{
+    class ExcerciseSet09
+    {
+        // Exercises
+        // 1- Write a program that reads a text file and displays the number of words.
+
+        // 2- Write a program that reads a text file and displays the longest word in the file.
+    }
+}
