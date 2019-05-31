148f7f79bafdd80621c6a9e5aec3db39c50416af
Aaron Sawyer <me@aarondevon.com>
2019-05-31 08:22:00 -0500

    Rename ExerciseSet06 to ExerciseSet05, update class references

--- Exercises/ExerciseSet05.cs
+++ Exercises/ExerciseSet05.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace Exercises
 {
-    class ExerciseSet06
+    class ExerciseSet05
     {
         // 01 - Write a program to count how many numbers between 1 and a number that are divisible by 3 with no remainder.Display the count on the console.
         public static void TotalNumbersDivisibleByThree(int topNumberRange)
--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -8,17 +8,17 @@ namespace Exercises
         {
             // Exercise Set 06*************************************
             // 01 - Write to the console the total numbers between 1 and 100 that are divisible by 3 with no remainder.
-            ExerciseSet06.TotalNumbersDivisibleByThree(100);
+            ExerciseSet05.TotalNumbersDivisibleByThree(100);
 
             // 02 - Ask the user to enter a number or "ok" to exit. Calculates the sum of all the previously entered numbers and displays it on the console.
-            ExerciseSet06.AskUserToEnterNumber();
+            ExerciseSet05.AskUserToEnterNumber();
 
             // 03 - Ask the user to enter a number. Compute the factorial of the number and print it on the console. For example, if the user enters 5, the program should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
 
-            ExerciseSet06.ComputeFactorialOfInt(5);
+            ExerciseSet05.ComputeFactorialOfInt(5);
 
             // 04 - Guess the radom number
-            ExerciseSet06.GuessTheNumber();
+            ExerciseSet05.GuessTheNumber();
         }
     }
 }
